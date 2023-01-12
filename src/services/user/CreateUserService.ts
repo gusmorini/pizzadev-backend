2a90e55e9d0ae0fd7bc0e159d1ae398a68f0a610
Gustavo Morini <gustavommh@gmail.com>
2023-01-12 16:32:53 -0300

    create user db

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import { hash } from "bcryptjs";
+import prismaClient from "../../prisma";
+
 interface UserRequest {
   name: string;
   email: string;
@@ -6,7 +9,33 @@ interface UserRequest {
 
 class CreateUserService {
   async execute({ name, email, password }: UserRequest) {
-    return { name, email, password };
+    // dados inválidos
+    if (!email || !name || !password)
+      throw new Error("invalid mandatory fields");
+
+    // email já cadastrado
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: { email },
+    });
+    if (userAlreadyExists) throw new Error("Email already exists");
+
+    const passwordHash = await hash(password, 8);
+
+    // cria usuario
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
   }
 }
 
