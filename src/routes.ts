6137085f14645fd1dd1c2c46181c4747b09651d9
Gustavo Morini <gustavommh@gmail.com>
2023-01-14 12:34:01 -0300

    list categories

--- src/routes.ts
+++ src/routes.ts
@@ -5,19 +5,24 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
+// --- USER ROUTES
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 
+// ___ CATEGORY ROUTES
 router.post(
   "/category",
   isAuthenticated,
   new CreateCategoryController().handle
 );
+
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
 
 export { router };
