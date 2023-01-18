6b544aad4e69d2a80b12af2db7cda160f1e85b78
Gustavo Morini <gustavommh@gmail.com>
2023-01-18 16:34:26 -0300

    fix create order

--- src/services/order/CreateOrderService.ts
+++ src/services/order/CreateOrderService.ts
@@ -7,16 +7,6 @@ interface OrderRequest {
 
 export class CreateOrderService {
   async execute({ table, name }: OrderRequest) {
-    const tableExists = await prismaClient.order.findFirst({
-      where: {
-        table: table,
-      },
-    });
-
-    if (tableExists) {
-      throw new Error("the table has already been opened");
-    }
-
     const order = await prismaClient.order.create({
       data: {
         table,
